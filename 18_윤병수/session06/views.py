2e12c6fd7b929ba017ba1d5c46110aec0a626389
윤병수 <qudtn990@likelion.org>
2021-06-24 00:01:45 +0900

    session06 assignment - 18_윤병수

---
+++ 18_윤병수/session06/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def count(request):
+    return render(request, 'count.html')
+
+def result(request):
+    text = request.POST['text']
+    total_len = len(text)
+    gongbaek = len(text.replace(" ", ""))
+    string = text.split(' ')
+
+    return render(request, 'result.html', {
+        'total_len' : total_len,
+        'gongbaek' : gongbaek,
+        'string' : string,
+    })
+
+def test(request):
+    return render(request, 'test.html')
+
+def test1(request):
+    return render(request, 'test1.html')
+
+def test2(request):
+    return render(request, 'test2.html')
